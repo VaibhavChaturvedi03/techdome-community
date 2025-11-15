4aa14b8f994950326840a66c236a345fa7d128bd
vaibhav chaturvedi <vaibhavchaturvedi.work@gmail.com>
2025-11-16 01:08:23 +0530

    adding files

---
+++ backend/models/userInfo.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const userInfoSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    message: {
+        type: String,
+        required: true,
+    },
+    subject:{
+        type: String,
+    },
+}, { timestamps: true });
+
+const User = mongoose.model('User', userInfoSchema);
+
+module.exports = User;
